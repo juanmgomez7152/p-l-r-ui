ad20d7be2fb8f31a4d283e590da925636c94bd1f
Juan Gomez Guzman <104452048+juanmgomez7152@users.noreply.github.com>
2024-11-06 11:00:55 -0600

    fix: update API URL in production environment configuration

--- src/environments/prod-environment.ts
+++ src/environments/prod-environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
     title: 'Para La Raza',
-    apiUrl: '/rest/api', 
+    apiUrl: 'https://p-l-r-api-service-332949365913.us-east1.run.app/rest/api', 
 };
